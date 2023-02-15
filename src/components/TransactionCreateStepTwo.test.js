9698892d740e717c2d006cdb796b867149f0dbcf
root <root@LAPTOP-VD7BUVAR>
2023-02-15 13:36:44 +0100

    finished integration test example

--- src/components/TransactionCreateStepTwo.test.js
+++ src/components/TransactionCreateStepTwo.test.js
@@ -1,27 +1,28 @@
-import { render, screen } from "@testing-library/react"
+import { ArrowRightOutlined } from "@material-ui/icons"
+import { render, screen, waitFor } from "@testing-library/react"
 import userEvent from "@testing-library/user-event"
 import  event  from "@testing-library/user-event"
+import { act } from "react-dom/test-utils"
 import TransactionCreateStepTwo from "./TransactionCreateStepTwo"
 
-test("on initial render, the pay button is disabled", async () => {
-    render(<TransactionCreateStepTwo sender={{id: "5"}} receiver={{id: "5"}}/>)
 
-    expect( await screen.findByRole("button", {name: /pay/i})).toBeDisabled();
-    //the name we see in the console the name for it is "pay"
-    //formik causes a bug because as you render the componen, the pay button goes from enabled to disabled
-
-    //when using async await you need to use findbyRole instead of getByRole
-    
-})
 
 
 test("if an amount and note is entered, the pay button becomes enabled", async () => {
     render(<TransactionCreateStepTwo sender={{id: "5"}} receiver={{id: "5"}}/>)
+    expect( await screen.findByRole("button", {name: /pay/i})).toBeDisabled();
     
-    userEvent.type(screen.getByPlaceholderText(/amount/i), "50");
-    userEvent.type(screen.getByPlaceholderText(/add a note/i), "dinner");
+    //we need to mimic the user event and we need to target it by the name .. we pass a number and then for the note, we pass dinner.
 
-    screen.getByRole("")
+        userEvent.type(screen.getByPlaceholderText(/amount/i), "50");
+        userEvent.type(screen.getByPlaceholderText(/add a note/i), "dinner");
 
+    
+    expect( await screen.findByRole("button", {name: /pay/i})).toBeEnabled();
     
 })
+
+// on the tests we have a pattern that consists of three steps:
+//1. Arrange ( where we render the component)
+//2. Act (user events, typing, clicking... things like that)
+//3. Assert ( where you make your assertions)
